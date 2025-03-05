f0f9fa2175202d67b7416356c1c69f2a19fa02b2
vitalichatyrka <vitalichatyrka@gmail.com>
2025-03-05 19:16:27 +0300

    test commit

---
+++ src/lesson_2/NewClass.java
@@ -0,0 +1,11 @@
+package lesson_2;
+
+public class NewClass {
+    public static void main(String[] args) {
+        getBassicVariables();
+    }
+
+    public static void getBassicVariables () {
+        System.out.println("NewClass.getBassicVariables");
+    }
+}

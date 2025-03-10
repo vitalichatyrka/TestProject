a4aa43660b59a69873117ff6e9471d9edc41b021
vitalichatyrka <vitalichatyrka@gmail.com>
2025-03-10 23:58:23 +0300

    add NumberSum feature

---
+++ src/lesson_3/NumberSum.java
@@ -0,0 +1,25 @@
+package lesson_3;
+
+import java.util.Scanner;
+
+public class NumberSum {
+    public static void main(String[] args) {
+        welcomeToNumberSumApp();
+        System.out.println("Sum of numbers " + getSum(inputData().nextInt()));
+    }
+    public static int getSum(int maxNumber) {
+        int sumOfNumbers = 0;
+        for (int startNumber = 1; startNumber <= maxNumber; startNumber++){
+            sumOfNumbers += startNumber;
+        }
+        return sumOfNumbers;
+    }
+    public static Scanner inputData(){
+        return new Scanner(System.in);
+    }
+    public static void welcomeToNumberSumApp() {
+        System.out.println("Welcome to NumberSum App");
+        System.out.print("Please enter the positive number: ");
+    }
+}
+

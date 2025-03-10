8ca6965847c91da8ca3d3cf55b1f82d7b224de77
vitalichatyrka <vitalichatyrka@gmail.com>
2025-03-11 00:01:28 +0300

    add PrintNumbersBySeven feature

---
+++ src/lesson_3/PrintNumbersBySeven.java
@@ -0,0 +1,16 @@
+package lesson_3;
+
+public class PrintNumbersBySeven {
+    public static void main(String[] args) {
+        printMultiplesOfSeven();
+
+    }
+   public static void printMultiplesOfSeven() {
+       int num = 7;
+       while (num <= 98) {
+           System.out.print(num + " ");
+           num += 7;
+       }
+   }
+}
+

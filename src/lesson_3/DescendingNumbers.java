c758f51a3b3ca1326928ec520ff3a26e53224769
vitalichatyrka <vitalichatyrka@gmail.com>
2025-03-11 00:04:45 +0300

    add DescendingNumbers feature

---
+++ src/lesson_3/DescendingNumbers.java
@@ -0,0 +1,14 @@
+package lesson_3;
+
+public class DescendingNumbers {
+    public static void main(String[] args) {
+        printNegativeSequence();
+    }
+    public static void printNegativeSequence() {
+        for (int num = 0, i = 0; i < 10; i++, num -= 5) {
+            System.out.print(num + " ");
+        }
+        }
+    }
+
+

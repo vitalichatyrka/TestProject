ac6e87cfb382b5d59e5abdc24408afdb5e0be81c
vitalichatyrka <vitalichatyrka@gmail.com>
2025-03-21 13:39:17 +0300

    add ChessboardSetup feature

---
+++ src/lesson_5/task_2/ChessboardSetup.java
@@ -0,0 +1,29 @@
+package lesson_5.task_2;
+
+public class ChessboardSetup {
+    char [][] array = new char[8][8];
+
+    ChessboardSetup(){
+        initializeChessboard();
+        printChessboard();
+    }
+
+    private void initializeChessboard(){
+for (int row = 0; row < 8; row++){
+    for (int col = 0; col < 8; col++){
+        array[row][col] = (row + col) % 2 == 0 ? 'W' : 'B';
+    }
+}
+    }
+    private void printChessboard (){
+        System.out.println("Your chessboard:");
+        for (char[] row : array) {
+            for (char cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
+    }
+    }
+
+
---
+++ src/lesson_5/task_2/Main.java
@@ -0,0 +1,8 @@
+package lesson_5.task_2;
+
+public class Main {
+    public static void main(String[] args) {
+        ChessboardSetup chessborad = new ChessboardSetup();
+    }
+
+}
